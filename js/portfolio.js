c79b1f9fc92bddeb36f851067f94abe21bb4d362
chrbour <108238838+chrbour@users.noreply.github.com>
2024-03-13 10:55:39 +0100

    Rename classes

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,24 +1,21 @@
 
-const mesProjets= document.getElementById('mesProjets');
-const lienProjet = document.getElementsByClassName('lienProjet');
+const mesProjets= document.getElementById('myProjects');
+const project__link = document.getElementsByClassName('project__link');
 const closingElement = document.getElementsByClassName('close');
 const redirection = document.getElementsByClassName('redirection');
 
-for (let i=0; i<lienProjet.length;i++){console.log(lienProjet[i]);
-    lienProjet[i].addEventListener("click",() => {
+for (let i = 0; i < project__link.length; i++){console.log(project__link[i]);
+    project__link[i].addEventListener("click",() => {
         mesProjets.style.visibility = "hidden";
-        console.log('image cliquée', mesProjets);
 })
 }
-for (let i=0; i<closingElement.length; i++){
+for (let i = 0; i < closingElement.length; i++){
     closingElement[i].addEventListener("click", () => {
         mesProjets.style.visibility = "visible";
-        console.log('close cliqué');
     })
 }
-for (let i=0; i<redirection.length; i++){
+for (let i = 0; i < redirection.length; i++){
     redirection[i].addEventListener("click", () => {
         mesProjets.style.visibility = "visible";
-        console.log('lien navbar cliqué');
     })
 }
